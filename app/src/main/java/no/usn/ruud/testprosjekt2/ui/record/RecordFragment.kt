6364401a69326aa3c3f5d95715576e833bbca7f4
Benjamin Nese <56982528+benjaminnese@users.noreply.github.com>
2021-10-24 12:22:10 +0200

    test

--- app/src/main/java/no/usn/ruud/testprosjekt2/ui/record/RecordFragment.kt
+++ app/src/main/java/no/usn/ruud/testprosjekt2/ui/record/RecordFragment.kt
@@ -8,12 +8,12 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import no.usn.ruud.testprosjekt2.databinding.FragmentDashboardBinding
+import no.usn.ruud.testprosjekt2.databinding.FragmentRecordBinding
 
 class RecordFragment : Fragment() {
 
     private lateinit var recordViewModel: RecordViewModel
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentRecordBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -27,7 +27,7 @@ class RecordFragment : Fragment() {
         recordViewModel =
             ViewModelProvider(this).get(RecordViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentRecordBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textDashboard
